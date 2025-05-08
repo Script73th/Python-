a60050e85ccfe474e185c525ce6e3eda2748c98f
Script73th <Leanon@bk.ru>
2025-05-08 15:49:42 +0900

    множества 1 задание

---
+++ Lessn 10 task1.py
@@ -0,0 +1,15 @@
+mn = set()
+num = int(input("Введите количесто чисел передаваемых в множество(больше 0 и меньше 100001)"))
+if(num<1) or (num>100000):
+    print("ошибка диапазона чисел")
+else:
+    value= int
+    for i in range(num):
+        print("ВВедите число до 2000000000 ")
+        value=input()
+        mn.add(value)
+    print(len(mn))    
+
+    
+
+
